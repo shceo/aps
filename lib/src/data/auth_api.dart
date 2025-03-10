9ece195f54c74ed9f12c93b5343328720cd3df73
shceo <fayzullojalolov42@gmail.com>
2025-03-10 22:32:13 +0500

    auth api fixed

--- lib/src/data/auth_api.dart
+++ lib/src/data/auth_api.dart
@@ -3,7 +3,11 @@ import 'package:dio/dio.dart';
 class ApiService {
   final Dio _dio = Dio(
     BaseOptions(
+<<<<<<< HEAD
       baseUrl: "http://127.0.0.1:8000/",  
+=======
+      baseUrl: "http://127.0.0.1:8000/api/",
+>>>>>>> f139bdf (auth api fixed)
       connectTimeout: const Duration(seconds: 5),
       receiveTimeout: const Duration(seconds: 5),
     ),
@@ -12,6 +16,7 @@ class ApiService {
   Future<Map<String, dynamic>> login(String username, String password) async {
     try {
       Response response = await _dio.post(
+<<<<<<< HEAD
         "api_login/",  
         data: {"username": username, "password": password},
       );
@@ -27,6 +32,15 @@ class ApiService {
     } catch (e) {
       print("Unexpected error: $e");
       return {"status": "error", "message": "Something went wrong"};
+=======
+        "login/",
+        data: {"username": username, "password": password},
+      );
+
+      return response.data; // JSON автоматически парсится
+    } catch (e) {
+      return {"status": "error", "message": "Login failed"};
+>>>>>>> f139bdf (auth api fixed)
     }
   }
 }
