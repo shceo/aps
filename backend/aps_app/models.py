37c9eab8373e44608933f2753524dd82d0a9aad1
Dilshod <dilshod1526@gmail.com>
2025-03-11 13:23:30 +0500

    model Account added

--- backend/aps_app/models.py
+++ backend/aps_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='User')
+    is_seller = models.BooleanField(default=False, verbose_name='Отправитель')
+    is_customer = models.BooleanField(default=True, verbose_name='Получатель')
+    email = models.EmailField(null=True, blank=True, verbose_name='Email')
+    phone = models.CharField(max_length=20, null=True, blank=True, verbose_name='Телефон')
+
+    class Meta:
+        verbose_name = "Аккаунт"
+        verbose_name_plural = "Аккаунты"
+        ordering = ['user']  # Orders by user
+
+    def __str__(self):
+        return f"{self.user.username} ({'Seller' if self.is_seller else 'Customer'})"
+
