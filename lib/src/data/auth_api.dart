436626fc5fafe1908ee1cb124faa6ba4fbe3b3cf
shceo <fayzullojalolov42@gmail.com>
2025-03-11 12:16:03 +0500

    auth

--- lib/src/data/auth_api.dart
+++ lib/src/data/auth_api.dart
@@ -3,44 +3,29 @@ import 'package:dio/dio.dart';
 class ApiService {
   final Dio _dio = Dio(
     BaseOptions(
-<<<<<<< HEAD
-      baseUrl: "http://127.0.0.1:8000/",  
-=======
       baseUrl: "http://127.0.0.1:8000/api/",
->>>>>>> f139bdf (auth api fixed)
       connectTimeout: const Duration(seconds: 5),
       receiveTimeout: const Duration(seconds: 5),
     ),
   );
 
-  Future<Map<String, dynamic>> login(String username, String password) async {
+  Future<Map<String, dynamic>> login(String phone, String password) async {
     try {
       Response response = await _dio.post(
-<<<<<<< HEAD
-        "api_login/",  
-        data: {"username": username, "password": password},
+        "api_login/",
+        data: {
+          "phone": phone,
+          "password": password,
+        },
       );
 
-      print("Login successful: ${response.data}");
       return response.data;
-    } on DioException catch (e) {
-      print("Dio error: ${e.response?.data ?? e.message}");
+    } catch (e) {
+      print("Ошибка входа: $e"); // Вывод ошибки в терминал
       return {
         "status": "error",
-        "message": e.response?.data["message"] ?? "Login failed"
+        "message": "Ошибка входа",
       };
-    } catch (e) {
-      print("Unexpected error: $e");
-      return {"status": "error", "message": "Something went wrong"};
-=======
-        "login/",
-        data: {"username": username, "password": password},
-      );
-
-      return response.data; // JSON автоматически парсится
-    } catch (e) {
-      return {"status": "error", "message": "Login failed"};
->>>>>>> f139bdf (auth api fixed)
     }
   }
 }
