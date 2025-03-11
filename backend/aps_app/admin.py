5ea4b24398dae06a3b444707ce136c374f2535aa
Dilshod <dilshod1526@gmail.com>
2025-03-11 13:24:02 +0500

    registering for the admin panel

--- backend/aps_app/admin.py
+++ backend/aps_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('user', 'is_seller', 'is_customer', 'email', 'phone')
+    list_filter = ('is_seller', 'is_customer')
+    search_fields = ('user__username', 'email', 'phone')
+    ordering = ('user',)
