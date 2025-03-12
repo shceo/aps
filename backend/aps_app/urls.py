44a3505afc615697011fb4dfbbbc63290f38ecbe
Dilshod <dilshod1526@gmail.com>
2025-03-12 12:42:08 +0500

    url path added for registration

--- backend/aps_app/urls.py
+++ backend/aps_app/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('login/admin/', login_view_admin, name='login_admin'),
-    path('logout/', logout_view, name='logout')
+    path('logout/', logout_view, name='logout'),
+    path('reg/', registration_view, name='reg')
 ]
