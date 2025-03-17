4809ed5d054167ce6c4d117b7a865c9d0fbfc7f1
Dilshod <dilshod1526@gmail.com>
2025-03-17 13:21:07 +0500

    JWT token generation enabled

--- backend/APS/urls.py
+++ backend/APS/urls.py
@@ -18,10 +18,18 @@ from django.contrib import admin
 from django.urls import path, include
 from . import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('aps_app.urls'))
+    path('', include('aps_app.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 
 if settings.DEBUG:
