5375ebea50ccb5d53d6eaf57c80cb3a4def5093a
shceo <fayzullojalolov42@gmail.com>
2025-03-21 20:01:28 +0500

    a

---
+++ android/app/build/intermediates/cxx/Debug/583k2qx7/logs/x86_64/configure_command.bat
@@ -0,0 +1,20 @@
+@echo off
+"E:\\itA\\andrstudiocomp\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HE:\\itA\\flutter\\packages\\flutter_tools\\gradle\\src\\main\\groovy" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=x86_64" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86_64" ^
+  "-DANDROID_NDK=E:\\itA\\andrstudiocomp\\Android\\Sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_ANDROID_NDK=E:\\itA\\andrstudiocomp\\Android\\Sdk\\ndk\\27.0.12077973" ^
+  "-DCMAKE_TOOLCHAIN_FILE=E:\\itA\\andrstudiocomp\\Android\\Sdk\\ndk\\27.0.12077973\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=E:\\itA\\andrstudiocomp\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=B:\\aps\\aps\\android\\app\\build\\intermediates\\cxx\\Debug\\583k2qx7\\obj\\x86_64" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=B:\\aps\\aps\\android\\app\\build\\intermediates\\cxx\\Debug\\583k2qx7\\obj\\x86_64" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BB:\\aps\\aps\\android\\app\\.cxx\\Debug\\583k2qx7\\x86_64" ^
+  -GNinja ^
+  -Wno-dev ^
+  --no-warn-unused-cli
